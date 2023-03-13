55774aa225eb980d61a889fe2d56f81e62ee1726
meloneng <melon_eng@protonmail.com>
2023-03-13 00:40:01 -0300

    can now solve loops

--- Remotagem/k-mers.py
+++ Remotagem/k-mers.py
@@ -1,11 +1,48 @@
 # Debug library
-#from icecream import ic
-#ic.configureOutput(prefix='Debug| ')
+from icecream import ic
+ic.configureOutput(prefix='Debug| ')
 #ic.disable()
+
+# Solve loops in the graph
+def solveLoop():
+  global DNA
+  global k
+  # Looking for a possible loop inside the DNA already formed
+  for idx, _ in ic(enumerate(DNA)):
+    if((idx+(k-1) > len(DNA))):
+      exit("Error: No loop found")
+    piece = DNA[idx:(idx+(k-1))]
+    if((piece in prefixes)):
+      lastMer = piece
+      nextMer = (prefixes[lastMer].pop(0))
+      if(not(prefixes[lastMer])):
+        del prefixes[lastMer]
+      DNAaux = nextMer[-1]
+      lastMer = nextMer
+      
+      # Building the DNA section that forms the loop
+      while((nextMer != piece)):
+        if(not(lastMer in prefixes)):
+          solveLoop()
+        nextMer = prefixes[lastMer].pop(0)
+        if(not(prefixes[lastMer])):
+          del prefixes[lastMer]
+        DNAaux = DNAaux + nextMer[-1]
+        lastMer = nextMer
+      
+      # Inserting the new loop section in the DNA already formed
+      DNA = DNA[:(idx+(k-1))] + DNAaux + DNA[(idx+(k-1)):]
+
+      break
+    else:
+      exit("Error: No loop found")
+
 
 # Opening file
 with open('input-50.txt') as f:
   content = f.read()
+
+k = 50
 
 # Creating an array with all the mers
 aux1 = ""
@@ -17,14 +54,14 @@ for i in content:
     continue
   aux1 = aux1 + i
 
+
 # Tamanho do mers
-k = 50
+
 
 # The dicts to be used
 prefixes = {}
-#prefixCount = {}
-#suffixCount = {}
 count = {}
+
 
 # Looping to create the prefix dict and check what is the first prefix and last suffix
 for i in mers:
@@ -32,18 +69,6 @@ for i in mers:
   suffix = i[1:]
 
   # Check if the key does not exist, if not creates it
-  '''
-  if(not((prefixes.get(prefix,False)))):
-    prefixes[prefix] = []
-  if(not((prefixCount.get(prefix,False)))):
-    prefixCount[prefix] = 0
-  if(not((suffixCount.get(suffix,False)))):
-    suffixCount[suffix] = 0
-
-  prefixes[prefix].append(suffix)
-  prefixCount[prefix] += 1
-  suffixCount[suffix] += 1
-  '''
   if(not((prefixes.get(prefix,False)))):
     prefixes[prefix] = []
   if(not((count.get(prefix,False)))):
@@ -58,6 +83,7 @@ for i in mers:
 
 # Defining what is the first prefix and last suffix inside sandf
 keysList = count.keys()
+'''start and finish'''
 sandf = ["",""]
 for i in keysList:
   if(count[i] == 1):
@@ -65,14 +91,19 @@ for i in keysList:
   if(count[i] == -1):
     sandf[1] = i
 
+
 # Build the DNA using the prefixes knowing the first and last mer
 nextMer = prefixes[sandf[0]].pop(0)
 if(not(prefixes[sandf[0]])):
   del prefixes[sandf[0]]
-DNA = nextMer
+DNA = sandf[0][0] + nextMer
 lastMer = nextMer
 
 while(prefixes):
+  #ic(DNA)
+  if(not(lastMer in prefixes)):
+    solveLoop()
+    break
   nextMer = prefixes[lastMer].pop(0)
   if(not(prefixes[lastMer])):
     del prefixes[lastMer]
@@ -80,7 +111,9 @@ while(prefixes):
   lastMer = nextMer
 
 
+# Write rebuild DNA on 'output.txt' file
 arquivo = open('output.txt','w')
 arquivo.write(DNA + '\n')
 arquivo.close()  
 
+#ic(DNA)
